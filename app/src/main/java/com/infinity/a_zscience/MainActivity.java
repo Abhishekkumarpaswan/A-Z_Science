e96d087f6b2d043ce82f07c6b3d4c56d8ddab7fe
Soul-Coder666 <johnwick6721@gmail.com>
2021-05-25 10:07:26 +0530

    A little change to check what happens

--- app/src/main/java/com/infinity/a_zscience/MainActivity.java
+++ app/src/main/java/com/infinity/a_zscience/MainActivity.java
@@ -2,6 +2,7 @@ package com.infinity.a_zscience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         chemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(this, chemistry_standard);
             }
         });
     }
