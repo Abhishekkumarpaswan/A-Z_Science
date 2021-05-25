752173017304635e862995fe870d1c1286d7bd94
Soul-Coder666 <72306155+Soul-Coder666@users.noreply.github.com>
2021-05-25 09:35:56 +0530

    Add files via upload

---
+++ AZScience/app/src/main/java/com/infinity/a_zscience/chemistry_standard.java
@@ -0,0 +1,14 @@
+package com.infinity.a_zscience;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chemistry_standard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chemistry_standard);
+    }
+}
